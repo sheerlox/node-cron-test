ad1cc7bd03ce06b372ec8cc97f3cbe3f5c1771cb
Pierre Cavin <me@sherlox.io>
2023-09-01 12:58:45 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { CronJob } from 'cron';
+
+const job = new CronJob(
+  '* * * * * *',
+  function() {
+      console.log('You will see this message every second');
+  },
+  null,
+  true,
+  'America/Los_Angeles'
+);
+job.start()
